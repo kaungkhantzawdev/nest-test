b406b3782c77bf664f36267497c5401d9a94125b
kaungkhantzawjudgify <kaung@judgify.me>
2023-09-25 14:03:34 +0630

    add more

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    MongooseModule.forRoot('mongodb://localhost/nest'),
+    CatsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
