e0636ba5e21d78bee6e6ea6a4c8c646987a2714c
kaungkhantzawjudgify <kaung@judgify.me>
2023-09-29 13:45:12 +0630

    add photo

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Express } from 'express';
+import { diskStorage } from 'multer';
+import path = require('path');
 
+const date = new Date();
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -8,5 +19,27 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @UseInterceptors(
+    FileInterceptor('photo', {
+      storage: diskStorage({
+        destination: './photos',
+        filename: (req, file, cb) => {
+          const filename: string =
+            path.parse(file.originalname).name.replace(/\s/g, '') +
+            '_photo_' +
+            date.getTime();
+          const extname: string = path.parse(file.originalname).ext;
+
+          cb(null, `${filename}${extname}`);
+        },
+      }),
+    }),
+  )
+  @Post('/upload')
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log('file', file);
+    return { img_path: file.filename };
   }
 }
