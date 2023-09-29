c8c12cf7ecda06a5e7bfd5252fee1b8baeccd62b
kaungkhantzawjudgify <kaung@judgify.me>
2023-09-29 15:21:12 +0630

    add more multiple file

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,10 +3,15 @@ import {
   Get,
   Post,
   UploadedFile,
+  UploadedFiles,
   UseInterceptors,
 } from '@nestjs/common';
 import { AppService } from './app.service';
-import { FileInterceptor } from '@nestjs/platform-express';
+import {
+  FileFieldsInterceptor,
+  FileInterceptor,
+  FilesInterceptor,
+} from '@nestjs/platform-express';
 import { Express } from 'express';
 import { diskStorage } from 'multer';
 import path = require('path');
@@ -24,7 +29,7 @@ export class AppController {
   @UseInterceptors(
     FileInterceptor('photo', {
       storage: diskStorage({
-        destination: './photos',
+        destination: './photos/single',
         filename: (req, file, cb) => {
           const filename: string =
             path.parse(file.originalname).name.replace(/\s/g, '') +
@@ -41,5 +46,59 @@ export class AppController {
   uploadFile(@UploadedFile() file: Express.Multer.File) {
     console.log('file', file);
     return { img_path: file.filename };
+  }
+
+  @UseInterceptors(
+    FilesInterceptor('photos', 3, {
+      storage: diskStorage({
+        destination: './photos/multiple',
+        filename: (req, file, cb) => {
+          const filename: string =
+            path.parse(file.originalname).name.replace(/\s/g, '') +
+            '_photos_' +
+            date.getTime();
+          const extname: string = path.parse(file.originalname).ext;
+
+          cb(null, `${filename}${extname}`);
+        },
+      }),
+    }),
+  )
+  @Post('/uploads')
+  uploaded(@UploadedFiles() files: Array<Express.Multer.File>) {
+    console.log('file', files);
+  }
+
+  @Post('upload-multiple')
+  @UseInterceptors(
+    FileFieldsInterceptor(
+      [
+        { name: 'avatar', maxCount: 1 },
+        { name: 'background', maxCount: 3 },
+      ],
+      {
+        storage: diskStorage({
+          destination: './photos/multiple-photos',
+          filename: (req, file, cb) => {
+            const filename: string =
+              path.parse(file.originalname).name.replace(/\s/g, '') +
+              '_multiple_' +
+              date.getTime();
+            const extname: string = path.parse(file.originalname).ext;
+
+            cb(null, `${filename}${extname}`);
+          },
+        }),
+      },
+    ),
+  )
+  uploadFileMultiple(
+    @UploadedFiles()
+    files: {
+      avatar?: Express.Multer.File[];
+      background?: Express.Multer.File[];
+    },
+  ) {
+    console.log(files);
   }
 }
